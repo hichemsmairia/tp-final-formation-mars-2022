0e8cdc3e502cd516803e63110a5cb65f62336f8d
devhichem <hichemsmairia22@gmail.com>
2022-03-15 20:34:07 +0100

    deuxieme changement

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import './App.css';
+import "./App.css";
 import React from "react";
-import Header from './components/Header';
-import Main from './components/Main';
-import Devices_Section from './components/Devices_Section';
-import Download_section from './components/Download_section';
-import Everywhere from './components/Everywhere';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Devices_Section from "./components/Devices_Section";
+import Download_section from "./components/Download_section";
+import Everywhere from "./components/Everywhere";
+import Footer from "./components/Footer";
+import Movies from "./components/Movies";
+import { Route, Routes } from "react-router-dom";
+
+
 function App() {
   return (
     <>
-     <Header/>
-     <Main/>
-     <Devices_Section/>
-     <Download_section/>
-     <Everywhere/>
-     <Footer/>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/devices" element={<Devices_Section />} />
+        <Route path="/download" element={<Download_section />} />
+        <Route path="/everywhere" element={<Everywhere />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
+
+      <Footer />
     </>
   );
 }
-
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,32 @@
 import React from "react";
-//importaion d'une image ! 
+//importaion d'une image !
 import img from "../assets/netflix.svg";
+
+import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="Header">
       <img src={img} alt="netflix logo" />
 
-      <a href="#"> Entrer </a>
+      <li>
+        <Link to="/">HOME</Link>{" "}
+      </li>
+      <li>
+        {" "}
+        <Link to="/devices">DEVICES</Link>
+      </li>
+      <li>
+        {" "}
+        <Link to="/download">DOWNLOAD</Link>
+      </li>
+      <li>
+        {" "}
+        <Link to="/everywhere">EVERYWHERE</Link>
+      </li>
+      <li>
+        {" "}
+        <Link to="/movies">MOVIES</Link>
+      </li>
     </header>
   );
 }
