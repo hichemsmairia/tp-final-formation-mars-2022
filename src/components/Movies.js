c11e9d0180ed6bb58c0f044a4e95073ad5c6859f
devhichem <hichemsmairia22@gmail.com>
2022-03-18 20:17:23 +0100

    derniere

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,22 +1,28 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
+import { Card, Button, Container, Row, Col } from "react-bootstrap";
 export default function Movies() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("avatar");
   const [movies, setMovies] = useState([]);
+
   const getMovies = () => {
     axios
       .get(
         `https://api.themoviedb.org/3/search/movie?api_key=ec0d0ac1a02925c493e247d245274ced&query=${query}`
       )
-      .then(function (response) {
-        setMovies(response.data.results);
+      .then((response) => {
+        setMovies(response.data.results.slice(0, 6));
       });
   };
 
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <main className="MainMovies">
-      <div >
+      <div>
         <label>
           {" "}
           recherche :
@@ -34,21 +40,28 @@ export default function Movies() {
         chaque element je vais affichier le titre 
         
         */}
-<div className="moviesList">
-        {movies.map((element) => {
-          return (
-            <span>
-              <h4>{element.original_title}</h4>
-
-              <img
-                style={{ width: "255px", height: "255px" }}
-                src={`https://image.tmdb.org/t/p/w500${element.poster_path}`}
-              />
-            </span>
-          );
-        })}
-        </div>
       </div>
+      <Container>
+        <Row xs={1} md={3}>
+          {movies.map((element) => {
+            return (
+              <Col>
+                <Card style={{ width: "18rem", margin: "25px" }}>
+                  <Card.Img
+                    variant="top"
+                    src={`https://image.tmdb.org/t/p/w500${element.poster_path}`}
+                  />
+                  <Card.Body>
+                    <Card.Title>{element.original_title}</Card.Title>
+                    <Card.Text>{element.overview.slice(0, 40)} ...</Card.Text>
+                    <Button variant="primary">continuez la lecture</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </main>
   );
 }
